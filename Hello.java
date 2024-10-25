e5c6abfbd1bb3be271b6e0da05da1ce377a60eee
try-angle3 <song01020@gmail.com>
2024-10-26 01:31:57 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+/*
+작성자: 빵창
+작성일: 2024.10.25
+ */
+
+public class Hello {
+//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello,java");
+	}//end of main
+}//end of class
